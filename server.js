3f0890e66c18e19c4159d847eba01738b28b49aa
hubert.hongois@gmail.com <hubert.hongois@gmail.com>
2019-05-03 11:35:58 +0200

    back 2

--- server.js
+++ server.js
@@ -1,26 +1,52 @@
-const Express = require("express");
-const BodyParser = require("body-parser");
-const mongoose = require("mongoose");
-var cors = require('cors');
-Connexion = require('./api/models/connexion.js');
+const bodyParser = require('body-parser');
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const path = require('path');
+const posts = require('./routes/postRoute');
+const users = require('./routes/userRoute');
+const dbURI = process.env.REACT_APP_DB_URI || require('./secrets').dbURI;
 
-const CONNECTION_URL = "mongodb+srv://hhongois:hhongois@cluster0-7faqc.gcp.mongodb.net/test?retryWrites=true";
+const app = express();
+const port = process.env.PORT || 5000;
 
+// Enable CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, PATCH, DELETE');
+    return res.status(200).json({});
+  }
+  return next();
+});
 
-var app = Express();
-
-app.use(BodyParser.json());
-app.use(BodyParser.urlencoded({ extended: false }));
-app.listen(4000,() => console.log(`Server running on port 4000`));
-
-app.use(cors());
 mongoose
-  .connect(CONNECTION_URL, {
-      useNewUrlParser:true
-  })
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+  .connect(
+    "mongodb+srv://hhongois:hhongois@cluster0-7faqc.gcp.mongodb.net/test?retryWrites=true",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.log('Failed to connect to MongoDB', err));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(logger('dev'));
+app.use('/posts', posts);
+app.use('/users', users);
 
- const routes = require('./api/routes/todoListRoutes')(app);
- 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, '..', 'client', 'build')));
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, '..', 'client', 'build', 'index.html')
+    );
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
+});
